c09dc9626359584eb02a8e189f2f149be55575dd
Hayden9898 <haydenc9898@gmail.com>
2025-03-06 23:58:21 -0500

    Update: created notification model to track follows and likes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const getUserProfile = async (req, res) => {
     const {username} = req.params;
@@ -19,8 +20,9 @@ export const followUnfollowUser = async (req, res) => {
         const { id } = req.params;
         const userToModify = await User.findById(id);
         const currentUser = await User.findById(req.user._id);
-
-        if(id === req.user._id){
+        
+        //Need to string, req is passed as object
+        if(id === req.user._id.toString()){
             return res.status(400).json({error: "You cannot follow/unfollow yourself"});
         }
 
@@ -28,7 +30,7 @@ export const followUnfollowUser = async (req, res) => {
             return res.status(400).json({ error: "User not found"})
         }
 
-        const isFollowing = currentUser.followers.includes(id);
+        const isFollowing = currentUser.following.includes(id);
         if(isFollowing){
             // Unfollow User
             await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id}});
@@ -39,6 +41,15 @@ export const followUnfollowUser = async (req, res) => {
             await User.findByIdAndUpdate(id, { $push: { followers: req.user._id}});
             await User.findByIdAndUpdate(req.user._id, { $push: { following: id}});
             // Send notification to user
+            const newNotification = new Notification({
+                type: "follow",
+                from: req.user._id,
+                to: userToModify._id,
+            });
+
+            await newNotification.save();
+
+            //TODO: return thew id of the user as a response
             res.status(200).json({ message: "User followed successfully"});
         }
     } catch (error) {
