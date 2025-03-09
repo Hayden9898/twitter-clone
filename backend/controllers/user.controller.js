874669db34e869831702e0c3d07d44343a6e4736
Hayden9898 <haydenc9898@gmail.com>
2025-03-08 23:28:53 -0500

    Update: Created suggested user and update user route. Configured cloudinary

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import Notification from "../models/notification.model.js";
 
@@ -55,5 +56,69 @@ export const followUnfollowUser = async (req, res) => {
     } catch (error) {
         console.log("Error in followUnfollowUser: ", error.message);
         res.status(500).json({error: error.message});
+    }
+}
+
+export const getSuggestedUser = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const usersFollowedByMe = await User.findById(userId).select("following");
+
+        const users = await User.aggregate([
+            {
+                $match:{
+                    _id: {$ne:userId}
+                }
+            },
+            {$sample:{size:10}}
+        ])
+
+        const filiteredUsers = users.filter(user=>!usersFollowedByMe.following.includes(user._id))
+        const suggestedUsers = filiteredUsers.slice(0,4);
+
+        suggestedUsers.forEach(user=>user.password=null);
+
+        res.status(200).json(suggestedUsers);
+    } catch (error) {
+        console.log("Error in getSuggestedUsers:", error.message);
+        res.status(500).json({error: error.message});
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const {fullName, email, username, currentPassword, newPassword, bio, link} = req.body;
+    let {profileImg, coverImg} = req.body;
+
+    const userId = req.user._id;
+
+    try {
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({ message: "User not found" });
+
+        if((!newPassword && currentPassword) || (newPassword && !currentPassword)){
+            res.stats(400).json({ error: "Pleasse provide both current password and new password"})
+        }
+
+        if(currentPassword && newPassword){
+            const isMatch = await bcrypt.compare(currentPassword, user.password);
+            if(!isMatch) return res.status(400).json({ error: "Current Password is incorrect"});
+            if(newPassword.length < 6){
+                return res.status(400).json({ error: "Password must be atleast 6 characters long"});
+            }
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(newPassword, salt);
+
+            if(profileImg){
+
+            }
+
+            if(coverImg){
+                
+            }
+        }
+    } catch (error) {
+        
     }
 }
