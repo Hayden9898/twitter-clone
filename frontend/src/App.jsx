b2f4bd394a006d516809f229f7a492bc805cab07
Hayden <haydenc9898@gmail.com>
2025-10-21 23:47:21 -0400

    feat: Created HomePage layout and sidebar component. Added sidebar to homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,14 @@ import{ Route,Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
 import LoginPage from "./pages/auth/login/LoginPage"
+import Sidebar from "./components/common/Sidebar"
 
 function App() {
   return (
     <>
       <div className='flex max-w-6xl mx-auto'>
+        {/* Common component, not wrapped in routes */}
+        <Sidebar /> 
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/signup' element={<SignUpPage />} />
