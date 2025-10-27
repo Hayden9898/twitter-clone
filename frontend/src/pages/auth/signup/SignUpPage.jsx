06043f8d0784d7d9ea4513e95366e7faf4b8c3dc
Hayden <haydenc9898@gmail.com>
2025-10-26 22:54:20 -0400

    refactor: simplify error handling for signUp page

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -29,8 +29,8 @@ const SignUpPage = () => {
 					body: JSON.stringify({email, username, fullName, password})
 				});
 				const data = await res.json()
-				if(!res.ok) throw new Error(data.error);
-				if(data.error) throw new Error(data.error);
+				if(!res.ok) throw new Error(data.error || "Failed to create account");
+				
 				console.log(data);
 				return data;
 			} catch (error) {
