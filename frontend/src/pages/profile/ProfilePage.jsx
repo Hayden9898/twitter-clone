2d831923c89ee8bb8ddc79720544c6e27558aab6
Hayden <haydenc9898@gmail.com>
2025-11-06 23:58:50 -0500

    feat: add update user profile and header functionality

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -11,8 +11,9 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, QueryClient, useQueryClient } from "@tanstack/react-query";
 import { formatMemberSinceDate } from "../../utils/db/date";
+import { toast } from "react-hot-toast";
 
 import useFollow from "../../hooks/useFollow";
 
@@ -28,6 +29,7 @@ const ProfilePage = () => {
 	const {username} = useParams();
 
 	const {follow, isPending} = useFollow();
+	const queryClient = useQueryClient();
 	const {data:authUser} = useQuery({queryKey: ["authUser"]})
 
 	const {data:user, isLoading, refetch, isRefetching} = useQuery({
@@ -43,6 +45,40 @@ const ProfilePage = () => {
 			} catch (error) {
 				throw new Error(error);
 			}
+		}
+	})
+
+	const{mutate:updateProfile, isPending:isUpdatingProfile} = useMutation({
+		mutationFn: async() => {
+			try {
+				const res = await fetch(`/api/users/update`, {
+					method: "POST",
+					headers:{
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						coverImg,
+						profileImg
+					})
+				})
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Profile updated successfully");
+			Promise.all([
+				queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+				queryClient.invalidateQueries({ queryKey: ["userProfile"]}),
+			])
+		},
+		onError: (error) => {
+			toast.error(error.message);
 		}
 	})
 
@@ -144,9 +180,9 @@ const ProfilePage = () => {
 								{(coverImg || profileImg) && (
 									<button
 										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
-										onClick={() => alert("Profile updated successfully")}
+										onClick={() => updateProfile()}
 									>
-										Update
+										{isUpdatingProfile ? "Updating...": "Save"}
 									</button>
 								)}
 							</div>
